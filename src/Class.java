f9f370148f2dcdec92687b1958f55c26a4c1d1cc
kimsangyub <48855393+kimsangyub@users.noreply.github.com>
2022-12-04 18:53:23 +0900

    Add files via upload

---
+++ src/Class.java
@@ -0,0 +1,17 @@
+package src;
+
+import java.util.HashMap;
+
+public class Class {
+    public int id;
+    public String name;
+    public Teacher teacher;
+    HashMap<Integer, Student> student_map = new HashMap<Integer, Student>();
+
+    public Class(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    
+}
